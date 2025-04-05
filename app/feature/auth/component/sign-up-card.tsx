2125d53f0651d237e2199588ff97eee1bcbbb5ca
Ruhan130 <ruhank170@gmail.com>
2025-04-05 11:29:38 +0500

    import form fields

--- app/feature/auth/component/sign-up-card.tsx
+++ app/feature/auth/component/sign-up-card.tsx
@@ -5,7 +5,27 @@ import { Input } from "@/components/ui/input";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import Link from "next/link";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormField, FormItem, FormMessage } from "@/components/ui/form";
+
+
+const formSchema = z.object({
+    email: z.string().email(),
+    password: z.string()
+});
+
 export const SignUpCard = () => {
+
+const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues:{
+        email: "",
+        password: ""
+    }
+})
+
     return (
         <Card className="w-full h-full md:w-[487px] border-none shadow-none">
             <CardHeader className="flex items-center justify-center text-center p-7">
