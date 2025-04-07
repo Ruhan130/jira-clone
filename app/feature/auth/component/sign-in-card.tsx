5a52f1a97bf683be0076eefa838f9bc053984465
Ruhan130 <ruhank170@gmail.com>
2025-04-07 13:36:15 +0500

    start working on sign up component

--- app/feature/auth/component/sign-in-card.tsx
+++ app/feature/auth/component/sign-in-card.tsx
@@ -10,10 +10,11 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import Link from "next/link";
 import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
 import { loginSchema } from "../schemas";
-
-
+import { useLogin } from "../api/use-login";
 
 export const SignInCard = () => {
+    const { mutate } = useLogin();
+
     const form = useForm<z.infer<typeof loginSchema>>({
         resolver: zodResolver(loginSchema),
         defaultValues: {
@@ -24,7 +25,7 @@ export const SignInCard = () => {
     });
 
     const onSubmit = (values: z.infer<typeof loginSchema>) => {
-        console.log({ values });
+        mutate({ json: values })
     };
     return (
         <Card className="w-full h-full md:w-[487px] border-none shadow-none">
--- app/feature/auth/server/route.ts
+++ app/feature/auth/server/route.ts
@@ -1,12 +1,13 @@
 import z from "zod"
 import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
+import { loginSchema } from "../schemas";
 
-const app = new Hono().post('/', zValidator("json", z.object({
-    email: z.string().email(),
-    password: z.string()
-})), (c) => {
-    return c.json({ success: "ok" });
+const app = new Hono().post("/login", zValidator("json", loginSchema), (c) => {
+
+    const { email, password } = c.req.valid("json");
+    console.log({ email, password });
+    return c.json({ email, password });
 });
 
 export default app;
