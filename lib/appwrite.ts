60e79761472c755d77f0571b8b76f5c1ec0aab10
Ruhan130 <ruhank170@gmail.com>
2025-04-09 11:24:31 +0500

    created appwrite

---
+++ lib/appwrite.ts
@@ -0,0 +1,19 @@
+import {
+    Client, Users, Account, Storage, Databases
+} from 'node-appwrite';
+
+
+export async function createAdminClient() {
+    const client = new Client()
+        .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+        .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!)
+        .setKey(process.env.NEXT_APPWRITE_KEY!)
+
+    return {
+        get Account() {
+            return new Account(client);
+        }
+    }
+
+
+}
