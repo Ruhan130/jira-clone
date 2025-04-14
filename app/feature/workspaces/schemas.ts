15b3e2853a0ebce1f46a36d8ffca0559f7dfed0c
Ruhan130 <ruhank170@gmail.com>
2025-04-14 12:31:59 +0500

    start creating storage in appwrite

--- app/feature/workspaces/schemas.ts
+++ app/feature/workspaces/schemas.ts
@@ -2,5 +2,10 @@ import { z } from "zod";
 
 export const createWrokspaceSchemas = z.object({
     name: z.string().trim().min(1, "Required"),
-    
+    imagUrl: z.union([
+        z.instanceof(File),
+        z.string().transform((value) => value === "" ? undefined : value),
+    ])
+        .optional(),
+
 });
