ec02c49cc3898e3677062a19f5f0ec9e00672c9e
Ruhan130 <ruhank170@gmail.com>
2025-04-15 15:34:56 +0500

    done

---
+++ app/(dashboard)/loading.tsx
@@ -0,0 +1,10 @@
+import { Loader } from "lucide-react"
+
+const DashboardLayout = () => {
+    return (
+        <div className="min-h-screen flex items-center justify-center ">
+            <Loader className="size-6 animate-spin text-muted-foreground" />
+        </div>
+    )
+}
+export default DashboardLayout
