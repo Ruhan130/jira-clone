b0816eb9024d5def97d941d950d6ce9648ca6e46
Ruhan130 <ruhank170@gmail.com>
2025-04-15 11:23:00 +0500

    creating switcher button

--- components/workspace-switcher.tsx
+++ components/workspace-switcher.tsx
@@ -1,9 +1,27 @@
 "use client"
+import { RiAddCircleFill } from "react-icons/ri";
+
+import { useGetWorkpsace } from "@/app/feature/workspaces/api/use-get-workspace"
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 export const WorkspaceSwitcher = () => {
+  const { data: workspace } = useGetWorkpsace();
   return (
-    <div> adnawndkdnwnd</div>
+    <div className="flex flex-col gap-y-2">
+      <div className="flex items-center justify-between">
+        <p className="text-xs uppercase text-neutral-500">
+          Workspaces
+        </p>
+        <RiAddCircleFill className="size-5 text-neutral-500 cursor-pointer hover:opacity-75 transition" />
+      </div>
+    </div>
   )
 }
 
-  
