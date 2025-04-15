8f2a557cf5701ce8161ea787775da16d524fd5a4
Ruhan130 <ruhank170@gmail.com>
2025-04-15 13:26:21 +0500

    go to last commit

--- app/feature/workspaces/server/route.ts
+++ app/feature/workspaces/server/route.ts
@@ -2,19 +2,38 @@ import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { createWrokspaceSchemas } from "../schemas";
 import { sessionMiddleware } from "@/lib/session-middleware";
-import { DATABASE_ID, IMAGE_BUCKET_ID, WORKSPACES_ID } from "@/config";
-import { ID } from "node-appwrite";
+import { DATABASE_ID, IMAGE_BUCKET_ID, MEMBERS_ID, WORKSPACES_ID } from "@/config";
+import { ID, Query } from "node-appwrite";
+import { MemberType } from "../../members/type";
 
 
 const app = new Hono()
     .get("/", sessionMiddleware, async (c) => {
         const databases = c.get("databases");
+        const user = c.get("user");
 
-        const worksapce = await databases.listDocuments(
+        const members = await databases.listDocuments(
+            DATABASE_ID,
+            MEMBERS_ID,
+            [Query.equal("userId", user.$id)]
+        );
+
+        if (members.total === 0) {
+            return c.json({ data: { documents: [], total: 0 } });
+        }
+
+        const workspaceIds = members.documents.map((memeber) => memeber.workspaceId);
+
+
+        const worksapces = await databases.listDocuments(
             DATABASE_ID,
             WORKSPACES_ID,
+            [
+                Query.orderDesc("createdAt"),
+                Query.contains("$id", workspaceIds)
+            ]
         );
-        return c.json({ data: worksapce });
+        return c.json({ data: worksapces });
     })
     .post("/", zValidator("form", createWrokspaceSchemas), sessionMiddleware, async (c) => {
         const databases = c.get("databases");
@@ -52,6 +71,17 @@ const app = new Hono()
                 imageUrl: uploadedImageUrl
             },
         );
+
+        await databases.createDocument(
+            DATABASE_ID,
+            MEMBERS_ID,
+            ID.unique(),
+            {
+                userId: user.$id,
+                workspaceId: workspaces.$id,
+                role: MemberType.ADMIN
+            }
+        )
 
         return c.json({ data: workspaces });
     }
