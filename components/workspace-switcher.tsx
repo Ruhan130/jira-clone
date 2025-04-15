a045438a563d605769bdfe5ba1d2e56572769c70
Ruhan130 <ruhank170@gmail.com>
2025-04-15 11:47:18 +0500

    done now start creating workspace avatar

--- components/workspace-switcher.tsx
+++ components/workspace-switcher.tsx
@@ -12,7 +12,7 @@ import {
 } from "@/components/ui/select";
 
 export const WorkspaceSwitcher = () => {
-  const { data: workspace } = useGetWorkpsace();
+  const { data: workspaces } = useGetWorkpsace();
   return (
     <div className="flex flex-col gap-y-2">
       <div className="flex items-center justify-between">
@@ -21,6 +21,18 @@ export const WorkspaceSwitcher = () => {
         </p>
         <RiAddCircleFill className="size-5 text-neutral-500 cursor-pointer hover:opacity-75 transition" />
       </div>
+      <Select>
+        <SelectTrigger className="w-full font-medium p-1 bg-neutral-200">
+          <SelectValue placeholder="No workspace Selected" />
+        </SelectTrigger>
+        <SelectContent>
+          {workspaces?.documents.map((workspaces) => (
+            <SelectItem key={workspaces.$id} value={workspaces.$id}>
+              {workspaces.name}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
     </div>
   )
 }
