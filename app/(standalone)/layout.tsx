21abe6b3a53c82513860a48001b882c0831b6067
Ruhan130 <ruhank170@gmail.com>
2025-04-16 12:42:09 +0500

    done with this branch

---
+++ app/(standalone)/layout.tsx
@@ -0,0 +1,30 @@
+import { Divide } from "lucide-react";
+import { UserButton } from "../feature/auth/component/user-button";
+import Link from "next/link";
+import Image from "next/image";
+
+interface StandAloneLayoutProps {
+    children: React.ReactNode;
+}
+const StandAloneLayout = ({ children }: StandAloneLayoutProps) => {
+    return (
+        <main>
+            <div className="bg-neutral-100 min-h-screen">
+                <div className="mx-auto max-w-screen-2xl p-4">
+                    <nav className="flex justify-between items-center h-[73px]">
+                        <Link href="/">
+                            <Image src="/logo.svg" alt="Logo" height={56} width={152} />
+                        </Link>
+                        <UserButton />
+
+                    </nav>
+                    <div className="flex flex-col items-center justify-center py-4">
+                        {children}
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export default StandAloneLayout
---
+++ app/(standalone)/workspaces/create/page.tsx
@@ -0,0 +1,15 @@
+import { getCurrent } from "@/app/feature/auth/actions";
+import { CreateWorkSpaceForm } from "@/app/feature/workspaces/component/create-workspace-form";
+import { redirect } from "next/navigation";
+
+const WorkspaceCreatePage = async () => {
+    const user = getCurrent();
+    if (!user) redirect("/sign-in");
+    return (
+        <div className="w-full lg:max-w-xl">
+            <CreateWorkSpaceForm />
+        </div>
+    )
+}
+
+export default WorkspaceCreatePage;
