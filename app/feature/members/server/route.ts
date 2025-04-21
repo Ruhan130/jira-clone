d82bbf659c97ade397ee8bf6c2bda52f53df3aa5
Ruhan130 <ruhank170@gmail.com>
2025-04-21 14:25:56 +0500

    server folder work done

--- app/feature/members/server/route.ts
+++ app/feature/members/server/route.ts
@@ -6,8 +6,10 @@ import { z } from "zod"
 
 import { Hono } from "hono"
 import { getMember } from "../utils"
-import { DATABASE_ID, WORKSPACES_ID } from "@/config"
+import { DATABASE_ID, MEMBERS_ID, WORKSPACES_ID } from "@/config"
 import { Query } from "node-appwrite"
+import { json } from "stream/consumers"
+import { MemberType } from "../type"
 
 const app = new Hono()
     .get("/",
@@ -56,6 +58,110 @@ const app = new Hono()
                     }
                 }
             )
+        }
+    )
+    .delete(
+        "/:memberId",
+        sessionMiddleware,
+        async (c) => {
+            const memberId = c.req.param("memberId");
+            const databases = c.get("databases");
+            const user = c.get("user");
+
+            const memberToDelte = await databases.getDocument(
+                DATABASE_ID,
+                MEMBERS_ID,
+                memberId,
+            );
+
+            const allMembersInfoWorkspace = await databases.listDocuments(
+                DATABASE_ID,
+                MEMBERS_ID,
+                [Query.equal("workspaceId", memberToDelte.$id)]
+            );
+
+            const member = await getMember(
+                {
+                    databases,
+                    workspaceId: memberToDelte.$id,
+                    userId: user.$id
+                }
+            );
+
+            if (!member) {
+                return c.json({ error: "Unauthorized" }, 401)
+            }
+            if (member.$id !== memberToDelte.$id && member.role !== MemberType.ADMIN) {
+                return c.json({ error: "Unauthorized" }, 401);
+            }
+
+            if (allMembersInfoWorkspace.total === 1) {
+                return c.json({ error: "Unauthorized" }, 400);
+            }
+
+            await databases.deleteDocument(
+                DATABASE_ID,
+                MEMBERS_ID,
+                memberId
+            );
+
+            return c.json({ data: { $id: memberToDelte.$id } });
+        }
+    )
+    .patch(
+        "/:memberId",
+        sessionMiddleware,
+        zValidator(
+            "json",
+            z.object({ role: z.nativeEnum(MemberType) })
+        ),
+        async (c) => {
+            const memberId = c.req.param("memberId");
+            const databases = c.get("databases");
+            const { role } = c.req.valid("json");
+            const user = c.get("user");
+
+            const memberToUpdate = await databases.getDocument(
+                DATABASE_ID,
+                MEMBERS_ID,
+                memberId,
+            );
+
+            const allMembersInfoWorkspace = await databases.listDocuments(
+                DATABASE_ID,
+                MEMBERS_ID,
+                [Query.equal("workspaceId", memberToUpdate.$id)]
+            );
+
+            const member = await getMember(
+                {
+                    databases,
+                    workspaceId: memberToUpdate.$id,
+                    userId: user.$id
+                }
+            );
+
+            if (!member) {
+                return c.json({ error: "Unauthorized" }, 401)
+            }
+            if (member.role !== MemberType.ADMIN) {
+                return c.json({ error: "Unauthorized" }, 401);
+            }
+
+            if (allMembersInfoWorkspace.total === 1) {
+                return c.json({ error: "Unauthorized" }, 400);
+            }
+
+            await databases.updateDocument(
+                DATABASE_ID,
+                MEMBERS_ID,
+                memberId,
+                {
+                    role
+                }
+            );
+
+            return c.json({ data: { $id: memberToUpdate.$id } });
         }
     )
 
