a801a52d305825fce0092034eceef11cb7704c36
Ruhan130 <ruhank170@gmail.com>
2025-04-22 11:25:36 +0500

    server component work done

---
+++ app/feature/projects/server/route.ts
@@ -0,0 +1,43 @@
+import { sessionMiddleware } from "@/lib/session-middleware";
+import { zValidator } from "@hono/zod-validator";
+import { Hono } from "hono";
+import { z } from "zod";
+import { getMember } from "../../members/utils";
+import { DATABASE_ID, PROJECTS_ID } from "@/config";
+import { Query } from "node-appwrite";
+
+const hono = new Hono()
+    .get(
+        "/",
+        sessionMiddleware,
+        zValidator("query", z.object({ workspaceId: z.string() })),
+        async (c) => {
+            const user = c.get("user");
+            const databases = c.get("databases");
+
+            const { workspaceId } = c.req.valid("query");
+
+
+            const member = await getMember({
+                databases,
+                workspaceId,
+                userId: user.$id
+            });
+
+            if (!member) {
+                return c.json({ error: "Unauthorized" }, 401);
+            }
+
+            const project = await databases.listDocuments(
+                DATABASE_ID,
+                PROJECTS_ID,
+                [
+                    Query.equal("workspaceId", workspaceId),
+                    Query.orderDesc("$createdAt")
+                ]
+            );
+            return c.json({ data: project });
+        }
+
+
+    )
