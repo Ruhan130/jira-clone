a9dbea49435f37111c5e9ceb175fcfe9b0e476f8
Ruhan130 <ruhank170@gmail.com>
2025-04-23 01:36:37 +0500

    working on project

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
 import { NavBar } from "@/components/navBar";
 import { Sidebar } from "@/components/sidebar";
 import { CreateResponsiveModal } from "../feature/workspaces/component/create-responsive-model";
+import { CreateProjectModal } from "../feature/projects/component/create-project-modal";
 
 interface DashboardLayoutProps {
     children: React.ReactNode;
@@ -10,6 +11,7 @@ const DashboardLayout = ({ children }: DashboardLayoutProps) => {
     return (
         <div className="min-h-screen">
             <CreateResponsiveModal />
+            <CreateProjectModal />
             <div className="flex w-full h-full">
                 <div className="fixed left-0 top-0 hidden lg:block lg:w-[264px] h-full overflow-y-auto">
                     <Sidebar />
--- app/feature/projects/api/use-create-project.ts
+++ app/feature/projects/api/use-create-project.ts
@@ -4,7 +4,7 @@ import { client } from "@/lib/rpc";
 import { toast } from "sonner";
 
 
-type ResponseType = InferResponseType<typeof client.api.projects["$post"]>;
+type ResponseType = InferResponseType<typeof client.api.projects["$post"], 200>;
 type RequestType = InferRequestType<typeof client.api.projects["$post"]>;
 
 export const useCreateProject = () => {
