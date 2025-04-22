1e505da9280d153ddeafeeacd16bc483eaf450fa
Ruhan130 <ruhank170@gmail.com>
2025-04-22 12:30:18 +0500

    changes in server routes

--- app/feature/projects/server/route.ts
+++ app/feature/projects/server/route.ts
@@ -3,8 +3,10 @@ import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { z } from "zod";
 import { getMember } from "../../members/utils";
-import { DATABASE_ID, PROJECTS_ID } from "@/config";
-import { Query } from "node-appwrite";
+import { DATABASE_ID, IMAGE_BUCKET_ID, PROJECTS_ID } from "@/config";
+import { ID, Query } from "node-appwrite";
+import { createProjectSchema } from "../schema";
+import { useGetMember } from "../../members/api/use-get-members";
 
 const app = new Hono()
     .get(
@@ -44,6 +46,65 @@ const app = new Hono()
         }
 
 
+    )
+    .post(
+        "/",
+        sessionMiddleware,
+        zValidator("form", createProjectSchema),
+        async (c) => {
+            const databases = c.get("databases");
+            const storage = c.get("storage");
+            const user = c.get("user");
+
+            const { name, image, workspaceId } = c.req.valid("form");
+
+            const member = await getMember(
+                {
+                    databases,
+                    workspaceId,
+                    userId: user.$id
+                }
+            );
+
+            if (!member) {
+                return c.json({ error: "Unauthorized" }, 401);
+            }
+
+            let uploadedImageUrl: string | undefined;
+
+            if (image instanceof File) {
+                const file = await storage.createFile(
+                    IMAGE_BUCKET_ID,
+                    ID.unique(),
+                    image,
+
+                );
+                const arryBuffer = await storage.getFileView(
+                    IMAGE_BUCKET_ID,
+                    file.$id
+                );
+                uploadedImageUrl = `data:image/png;base64,${Buffer.from(arryBuffer).toString("base64")}`;
+            }
+
+
+
+            const project = await databases.createDocument(
+                DATABASE_ID,
+                PROJECTS_ID,
+                ID.unique(),
+                {
+                    // KEY POINT EXACT SAME NAAM HOGA JO APPWRITE K ATTRIBUTES M HAIN
+                    name,
+                    userId: user.$id,
+                    imageUrl: uploadedImageUrl,
+                    workspaceId
+                },
+            );
+
+
+
+            return c.json({ data: project });
+        }
     )
 
 export default app 
