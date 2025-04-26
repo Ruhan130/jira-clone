5e49489039e0a7f8a43217fbb6189eec60da4f67
Ruhan130 <ruhank170@gmail.com>
2025-04-26 12:34:03 +0500

    b

--- app/(dashboard)/workspaces/[workspaceId]/page.tsx
+++ app/(dashboard)/workspaces/[workspaceId]/page.tsx
@@ -1,11 +1,14 @@
 import { getCurrent } from "@/app/feature/auth/queries";
 import { redirect } from "next/navigation";
 
-const WorkspaceIdPage = async ({ params }) => {
+const WorkspaceIdPage = async () => {
     const user = await getCurrent();
     if (!user) redirect("/sign-in");
     return (
-        <div className="">Workspace id: {params.workspaceId}</div>
+        <div className="">
+            
+        </div>
+        // <div className="">Workspace id: {params.workspaceId}</div>
     )
 }
 export default WorkspaceIdPage;
--- app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
+++ app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
@@ -1,6 +1,8 @@
 import { getCurrent } from "@/app/feature/auth/queries";
 import { ProjectAvatar } from "@/app/feature/projects/component/create-project-avatar";
 import { getProject } from "@/app/feature/projects/queries";
+import { TaskViewSwitcher } from "@/app/feature/tasks/components/task-view-switcher";
+// import TaskViewSwitcher from "@/app/feature/tasks/components/task-view-switcher";
 import { Button } from "@/components/ui/button";
 import { PencilIcon } from "lucide-react";
 import Link from "next/link";
@@ -41,6 +43,7 @@ const ProjectIdPage = async ({ params }: ProjectIdpAgeProps) => {
                     </Button>
                 </div>
             </div>
+            <TaskViewSwitcher />
         </div>
     );
 }
