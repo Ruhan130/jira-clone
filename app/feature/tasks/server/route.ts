dff021cdda55fb0b5b4f03ad38ca061e7dbcdae4
Ruhan130 <ruhank170@gmail.com>
2025-04-26 14:51:17 +0500

    start creating .get api

--- app/feature/tasks/server/route.ts
+++ app/feature/tasks/server/route.ts
@@ -1,5 +1,75 @@
+import { sessionMiddleware } from "@/lib/session-middleware";
+import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
+import { createTaskSchenma } from "../schemas";
+import { getMember } from "../../members/utils";
+import { DATABASE_ID, TASKS_ID } from "@/config";
+import { ID, Query } from "node-appwrite";
 
-const app = new Hono();
+const app = new Hono()
+    .post(
+        "/",
+        sessionMiddleware,
+        zValidator("json", createTaskSchenma),
+        async (c) => {
+            const user = c.get("user");
+            const databases = c.get("databases");
+            const {
+                name,
+                status,
+                workspaceId,
+                projectId,
+                dueDate,
+                assigneeId,
+                description
+            } = c.req.valid("json");
+
+
+            const member = await getMember({
+                databases,
+                workspaceId,
+                userId: user.$id
+            });
+
+            if (!member) {
+                return c.json({ error: "Unauthorized" }, 401);
+            }
+
+            const hightestPositionTask = await databases.listDocuments(
+                DATABASE_ID,
+                TASKS_ID,
+                [
+                    Query.equal("status", status),
+                    Query.equal("worksapceId", workspaceId),
+                    Query.orderAsc("positon"),
+                    Query.limit(1)
+                ]
+            );
+
+            const newPosition =
+                hightestPositionTask.documents.length > 0 ? hightestPositionTask.documents[0].position + 1000 : 1000;
+
+            const task = await databases.createDocument(
+                DATABASE_ID,
+                TASKS_ID,
+                ID.unique(),
+                {
+                    name,
+                    status,
+                    workspaceId,
+                    projectId,
+                    dueDate,
+                    assigneeId,
+                    description,
+                    position: newPosition
+                }
+
+            );
+
+            return c.json({ data: task });
+
+
+        }
+    )
 
 export default app
