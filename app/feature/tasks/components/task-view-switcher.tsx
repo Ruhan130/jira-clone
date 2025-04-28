46dda1d7ba687e8e8ac284ce8535fc3c8ee01ee7
Ruhan130 <ruhank170@gmail.com>
2025-04-28 11:08:54 +0500

    start creating form

--- app/feature/tasks/components/task-view-switcher.tsx
+++ app/feature/tasks/components/task-view-switcher.tsx
@@ -4,38 +4,40 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { PlusIcon } from "lucide-react"
 
 export const TaskViewSwitcher = () => {
-    return <Tabs className="flex-1 w-full border rounded-lg mt-4">
-        <div className="h-full flex flex-col overflow-auto p-4">
-            <div className="flex flex-col gap-y-2 lg:flex-row justify-between">
-                <TabsList className="w-full lg:w-auto">
-                    <TabsTrigger value="table" className="h-8 w-full lg:w-auto">
-                        Table
-                    </TabsTrigger>
-                    <TabsTrigger value="kanban" className="h-8 w-full lg:w-auto">
-                        kaban
-                    </TabsTrigger>
-                    <TabsTrigger value="calender" className="h-8 w-full lg:w-auto">
-                        Calender
-                    </TabsTrigger>
-                </TabsList>
-                <Button className="w-full lg:w-auto" size="sm" variant="primary">
-                    <PlusIcon className="size-4 mr-2" />
-                    New
-                </Button>
+    return (
+        <Tabs className="flex-1 w-full border rounded-lg">
+            <div className="h-full flex flex-col overflow-auto p-4">
+                <div className="flex flex-col gap-y-2 lg:flex-row  justify-between items-center">
+                    <TabsList className="w-full lg:w-auto">
+                        <TabsTrigger value="table" className="h-8 w-full lg:w-auto">
+                            Table
+                        </TabsTrigger>
+                        <TabsTrigger value="kanban" className="h-8 w-full lg:w-auto">
+                            kaban
+                        </TabsTrigger>
+                        <TabsTrigger value="calender" className="h-8 w-full lg:w-auto">
+                            Calender
+                        </TabsTrigger>
+                    </TabsList>
+                    <Button className="w-full lg:w-auto" size="sm">
+                        <PlusIcon className="size-4 mr-2" />
+                        New
+                    </Button>
+                </div>
                 <DottedSeperator className="my-4" />
                 DATA FILTER
                 <DottedSeperator className="my-4" />
-                <TabsContent value="table" className="my-0">
+                <TabsContent value="table" className="mt-0">
                     Table
                 </TabsContent>
-                <TabsContent value="kanban" className="my-0">
+                <TabsContent value="kanban" className="mt-0">
                     kanban
                 </TabsContent>
-                <TabsContent value="calender" className="my-0">
+                <TabsContent value="calender" className="mt-0">
                     calender
                 </TabsContent>
 
             </div>
-        </div>
-    </Tabs>
+        </Tabs>
+    )
 }
