36e11f73f76aa6bfee9856460688acf6a81d6e53
Ruhan130 <ruhank170@gmail.com>
2025-04-28 11:48:28 +0500

    loading feature done on create Task Form wrapper

---
+++ app/feature/tasks/components/creat-task-form-wrapper.tsx
@@ -0,0 +1,38 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { useGetMember } from "../../members/api/use-get-members";
+import { useGetProjects } from "../../projects/api/use-get-projects";
+import { UseWorkspaceId } from "../../workspaces/hooks/use-workspace-id";
+import { Loader } from "lucide-react";
+
+interface CreateTaskWrapperProps {
+    onCanel: () => void;
+}
+
+export const CreateTaskFormWRapper = ({ onCanel }: CreateTaskWrapperProps) => {
+    const workspaceId = UseWorkspaceId();
+    const { data: members, isLoading: isLoadingMembers } = useGetMember({ workspaceId });
+    const { data: projects, isLoading: isLoadingProjects } = useGetProjects({ workspaceId });
+
+    const projectOptions = projects?.documents.map((projects) => {
+        id: projects.$id;
+        name: projects.name;
+        imageUrl: projects.imageUrl;
+    });
+
+    const memberOptions = members?.documents.map((members) => {
+        id: members.$id;
+        name: members.name;
+    });
+
+    const isLoading = isLoadingProjects || isLoadingMembers;
+
+    if (isLoading)
+        return (
+            <Card className="w-full h-[714px] shadow-none border-none">
+                <CardContent className="flex  items-center justify-center h-full">
+                    <Loader className="size-8 animate-spin text-muted-foreground" />
+                </CardContent>
+            </Card>
+        )
+
+}
