b40fc1d8f7841e1f72050f5155cea2bc543de364
Ruhan130 <ruhank170@gmail.com>
2025-04-30 16:51:24 +0500

    work done created successfully

--- app/feature/tasks/components/data-calendar.tsx
+++ app/feature/tasks/components/data-calendar.tsx
@@ -14,6 +14,8 @@ import { useState } from "react";
 import "react-big-calendar/lib/css/react-big-calendar.css"
 import "./data-calendar.css"
 import { EventCard } from "./event-card";
+import { Button } from "@/components/ui/button";
+import { CalendarIcon, ChevronLeft, ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 
 
 const locales = {
@@ -30,6 +32,40 @@ const localizer = dateFnsLocalizer({
 
 interface DataCalendarProps {
     data: Task[]
+}
+interface CustomtoolBarPops {
+    date: Date;
+    onNavigate: (action: "PREV" | "NEXT") => void
+}
+export const CustomtoolBar = ({
+    date, onNavigate
+}: CustomtoolBarPops) => {
+    return (
+        <div className="flex mb-4 gap-x-2 items-center w-full lg:w-auto justify-center lg:justify-start">
+            <Button
+                onClick={() => onNavigate("PREV")}
+                variant="secondary"
+                size="icon"
+
+            >
+                <ChevronLeftIcon className="size-4" />
+            </Button>
+
+            <div className="flex items-center border border-input rounded-md px-3 py-2 h-8 justify-center w-full lg:w-auto">
+                <CalendarIcon className="size-4 mr-2" />
+                <p className="text-sm"> {format(date, "MMMM yyyy")}</p>
+            </div>
+
+            <Button
+                onClick={() => onNavigate("NEXT")}
+                variant="secondary"
+                size="icon"
+
+            >
+                <ChevronRightIcon className="size-4" />
+            </Button>
+        </div>
+    )
 }
 
 export const DataCalendar = ({
@@ -84,6 +120,9 @@ export const DataCalendar = ({
                             status={event.status}
 
                         />
+                    ),
+                    toolbar: () => (
+                        <CustomtoolBar date={value} onNavigate={handleNavigate} />
                     )
                 }
             }
