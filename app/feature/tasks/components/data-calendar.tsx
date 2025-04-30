e8cce83d42e06c7e6478f52117f4aa8e1c6b3d35
Ruhan130 <ruhank170@gmail.com>
2025-04-30 14:53:19 +0500

    fetch

---
+++ app/feature/tasks/components/data-calendar.tsx
@@ -0,0 +1,15 @@
+import { Task } from "../types";
+
+interface DataCalendarProps {
+    data: Task[]
+}
+
+export const DataCalendar = ({
+    data    
+}: DataCalendarProps) => {
+    return (
+        <div>
+            Data CaLendar
+        </div>
+    )
+}
