34538d12585020975121cd53408ae83b2e3bc76a
Ruhan130 <ruhank170@gmail.com>
2025-05-02 11:35:53 +0500

    task page found

--- app/(dashboard)/workspaces/[workspaceId]/task/[taskId]/client.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-export const TaskIdClient = () => {
-    return (
-        <div >
-            Task Id client
-        </div>
-    );
-}
-
---
+++ app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useGetTask } from "@/app/feature/tasks/api/use-get-task";
+import { useTaskId } from "@/app/feature/tasks/hooks/use-task-id";
+import { PageError } from "@/components/page-error";
+import { PageLoader } from "@/components/page-loader";
+
+
+export const TaskIdClient = () => {
+    const taskId = useTaskId();
+    const { data, isLoading } = useGetTask({ taskId });
+    if (isLoading) {
+        return <PageLoader />
+    }
+    if (!data) {
+        return <PageError message="Task no found" />
+    }
+
+    return (
+        <p>
+            sas
+            {JSON.stringify(data)}
+        </p>
+
+
+    );
+}
+
